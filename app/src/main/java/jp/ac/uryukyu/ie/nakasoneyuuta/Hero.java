5c2d25c4d438085e1de496b98327642453392ee9
仲宗根悠太 <nakasoneyuuta@nakasoneyuutanoMacBook-Air.local>
2020-12-08 15:00:23 +0900

    Implement Super Class

--- app/src/main/java/jp/ac/uryukyu/ie/nakasoneyuuta/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/nakasoneyuuta/Hero.java
@@ -6,21 +6,8 @@ package jp.ac.uryukyu.ie.nakasoneyuuta;
  * ヒーロークラス。 String name; //敵の名前 int hitPoint; //敵のHP int attack; //敵の攻撃力 boolean
  * dead; //敵の生死状態。true=死亡。 Created by tnal on 2016/11/13.
  */
-public class Hero {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
-
-    /**
-     * クラス外からヒーローの名前を取得するためのメソッド
-     * 
-     * @return name ヒーローの名前
-     */
-    public String getName() {
-        return this.name;
-    }
-
+public class Hero extends LivingThing{
+    
     /**
      * クラス外からヒーローのHPを取得するためのメソッド
      * 
@@ -39,39 +26,8 @@ public class Hero {
         return this.attack;
     }
 
-    /**
-     * クラス外からヒーローの生死状態を取得するためのメソッド
-     * 
-     * @return dead ヒーローの生死状態
-     */
-    public boolean getDead() {
-        return this.dead;
-    }
-
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * 
-     * @param name      ヒーロー名
-     * @param maximumHP ヒーローのHP
-     * @param attack    ヒーローの攻撃力
-     */
-    public Hero(String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * Enemyへ攻撃するメソッド。 attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * 
-     * @param e 攻撃対象
-     */
-    public void attack(Enemy e) {
-        int damage = (int) (Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
+    public Hero(String name, int hitPoint, int attack){
+        super(name, hitPoint, attack);
     }
 
     /**
@@ -79,9 +35,10 @@ public class Hero {
      * 
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage) {
         hitPoint -= damage;
-        if (hitPoint < 0) {
+        if (hitPoint <= 0) {
             dead = true;
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
