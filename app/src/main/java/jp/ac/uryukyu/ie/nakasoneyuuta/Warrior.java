dd23077d510d9f61bc74dba7299d00bc26445701
仲宗根悠太 <nakasoneyuuta@nakasoneyuutanoMacBook-Air.local>
2020-12-08 16:55:10 +0900

    Implement Warrior Class

---
+++ app/src/main/java/jp/ac/uryukyu/ie/nakasoneyuuta/Warrior.java
@@ -0,0 +1,14 @@
+package jp.ac.uryukyu.ie.nakasoneyuuta;
+
+public class Warrior extends Hero{
+    public Warrior(String name, int hitPoint, int attack){
+        super(name, hitPoint, attack);
+    }
+    public void attackWithWeponSkill(LivingThing opponent){
+        if(this.dead == false){
+            int damage = (int) (attack * 1.5);
+            System.out.printf("%sの攻撃！ウェポンスキルを発動！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+}
